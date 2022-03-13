b5373a27ec9be589a8704d0f10db88b7796e6a36
Siaka MANSALY <siaka.mansaly@gmail.com>
2022-03-13 02:33:12 +0100

    add route trick page

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -5,10 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Trick;
-use App\Repository\TrickRepository;
-use App\Repository\CommentRepository;
-use App\Repository\UserRepository;
-use App\Repository\TypeTrickRepository;
+
 
 class TricksController  extends AbstractController
 {
@@ -21,5 +18,14 @@ class TricksController  extends AbstractController
         $tricksAll = $tricks->findAll();
 
         return $this->render('home/index.html.twig', ['tricks' => $tricksAll]);
+    }
+
+    /**
+     * @Route("/trick/{id}", name="trickShow")
+     */
+    public function trickShow(Trick $trick)
+    {
+        dump($trick);
+        return $this->render('single/index.html.twig', ['trick' => $trick]);
     }
 }
