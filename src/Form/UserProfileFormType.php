0e9c0ca56ee7fc008819ba20d3ce675ba08701fd
Siaka MANSALY <siaka.mansaly@gmail.com>
2022-03-16 17:01:42 +0100

    make edit user

--- src/Form/UserProfileFormType.php
+++ src/Form/UserProfileFormType.php
@@ -8,7 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 
-class UserPreferencesFormType extends AbstractType
+class UserProfileFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
