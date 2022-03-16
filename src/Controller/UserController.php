de68576b9fff3e6c4941885ce979502c6e245ee5
Siaka MANSALY <siaka.mansaly@gmail.com>
2022-03-16 09:10:30 +0100

    fix bug picture null

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -30,26 +30,29 @@ class UserController extends AbstractController
     public function userPreferences(User $user, Request $request, UserRepository $userRepository, FileUploader $fileUploader): Response
     {
         $user = $userRepository->find($user);
+
         $oldPicture = $user->getPicture();
         $form = $this->createForm(UserPreferencesFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             //$user->setToken($token);
-            
+
             $picture = $form->get('picture')->getData();
-        if ($picture) {
-            $pictureFileName = $fileUploader->upload($picture);
-            $user->setPicture($pictureFileName);
-        }
-            $filesystem = new Filesystem();
-            $filesystem->remove($this->getParameter('profiles_directory').'/'.$oldPicture);
+            if ($picture) {
+                $pictureFileName = $fileUploader->upload($picture);
+                $user->setPicture($pictureFileName);
+            }
+            if ($oldPicture) {
+                $filesystem = new Filesystem();
+                $filesystem->remove($this->getParameter('profiles_directory') . '/' . $oldPicture);
+            }
             $userRepository->add($user);
             // Add message Flash and redirect to home
             $this->addFlash('success', "Update done !");
-            return $this->redirectToRoute('app_user_edit',['id'=>$user->getId()]);
+            return $this->redirectToRoute('app_user_edit', ['id' => $user->getId()]);
         }
-        $pictureTemp = $this->getParameter('profiles_directory_path').'/'.$user->getPicture();
+        $pictureTemp = $this->getParameter('profiles_directory_path') . '/' . $user->getPicture();
         return $this->render('security/user_preferences.html.twig', ['userPreferencesForm' => $form->createView(), 'picture' => $pictureTemp]);
     }
 }
