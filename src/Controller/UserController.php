6b44cd6ff923f977fbe74ebaaf61695f9b7f468a
Siaka MANSALY <siaka.mansaly@gmail.com>
2022-03-16 17:00:13 +0100

    make delete user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,7 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\UserPreferencesFormType;
+use App\Form\UserProfileFormType;
+use App\Form\UserRoleFormType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,24 +16,71 @@ use Symfony\Component\Filesystem\Filesystem;
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="app_user")
+     * @Route("/admin/users", name="app_users")
      */
-    public function index(): Response
+    public function adminUsers(UserRepository $users): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        return $this->render('user/index.html.twig', ['users' => $users->findAll()]);
     }
 
     /**
-     * @Route("/user/{id}/edit", name="app_user_edit")
+     * @Route("/admin", name="app_admin_index")
      */
-    public function userPreferences(User $user, Request $request, UserRepository $userRepository, FileUploader $fileUploader): Response
+    public function admin(UserRepository $users): Response
+    {
+        return $this->render('admin/index.html.twig');
+    }
+
+    /**
+     * @Route("/admin/user/{id}/edit_role", name="app_user_role")
+     */
+    public function adminUserRole(User $user, Request $request, UserRepository $userRepository): Response
+    {
+        $form = $this->createForm(UserRoleFormType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $userRepository->add($user);
+            $message = " The role of ".$user->getFirstname()." ".$user->getLastname()." has changed successfully !!";
+            $this->addFlash('success',$message);
+            return $this->redirectToRoute('app_users');
+        }
+        
+        return $this->render('user/role.html.twig', ['userRoleForm' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/admin/user/{id}/delete", name="app_user_delete")
+     */
+    public function adminUserDelete(User $user, Request $request, UserRepository $userRepository): Response
     {
         $user = $userRepository->find($user);
 
+        if($request->request->count()>0) {
+
+            $userRepository->remove($user);
+            $message = " The account of ".$user->getFirstname()." ".$user->getLastname()." has deleted successfully !!";
+            $this->addFlash('success',$message);
+            return $this->redirectToRoute('app_users');
+        }
+        
+        return $this->render('user/delete.html.twig', ['user' => $user]);
+    }
+
+    /**
+     * @Route("/profile/user/{id}/edit", name="app_user_edit")
+     */
+    public function profileUser(User $user, Request $request, UserRepository $userRepository, FileUploader $fileUploader): Response
+    {
+        $user = $userRepository->find($user);
+        $pictureTemp = "";
+        //
+        if($this->isGranted('ROLE_ADMIN')==false && $this->getUser()->getUserIdentifier()!=$user->getEmail())
+        {
+            return $this->redirectToRoute('index');
+        }
         $oldPicture = $user->getPicture();
-        $form = $this->createForm(UserPreferencesFormType::class, $user);
+        $form = $this->createForm(UserProfileFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -52,7 +100,15 @@ class UserController extends AbstractController
             $this->addFlash('success', "Update done !");
             return $this->redirectToRoute('app_user_edit', ['id' => $user->getId()]);
         }
-        $pictureTemp = $this->getParameter('profiles_directory_path') . '/' . $user->getPicture();
-        return $this->render('security/user_preferences.html.twig', ['userPreferencesForm' => $form->createView(), 'picture' => $pictureTemp]);
+        
+        if($user->getPicture())
+        {
+            $pictureTemp = $this->getParameter('profiles_directory_path') . '/' . $user->getPicture();
+        }
+        $page = "false";
+        if($request->get('d')){
+            $page = "true";
+        }
+        return $this->render('user/profile.html.twig', ['userPreferencesForm' => $form->createView(), 'picture' => $pictureTemp, 'page' => $page]);
     }
 }
