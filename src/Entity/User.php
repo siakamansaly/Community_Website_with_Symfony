ebc7e6479d03261a097606f0c247b4a15b37d24f
Siaka MANSALY <siaka.mansaly@gmail.com>
2022-03-23 18:16:32 +0100

    delete comment unuse

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -361,8 +361,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function eraseCredentials()
     {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+
     }
 
     public function getPicture(): ?string
